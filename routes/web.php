62ac4771dde10c4af6a33891634d135cbda33beb
Suparerk <ch.suparerk@gmail.com>
2019-12-16 11:37:10 +0700

    Pass request data to views

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,10 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/test', function () {
+    $name = request('name');
+    return view('test', [
+        'name' => $name
+    ]);
+});
