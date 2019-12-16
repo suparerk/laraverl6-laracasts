95f84e2b91b00a5cb2194aaba3bbc15cedd20a80
Suparerk <ch.suparerk@gmail.com>
2019-12-16 11:47:58 +0700

    Route Wildcards

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,18 @@ Route::get('/test', function () {
         'name' => $name
     ]);
 });
+
+Route::get('/posts/{post}', function ($post) {
+    $posts = [
+        'my-first-post' => 'Hello this is my first blog post!',
+        'my-second-post' => 'This is the second one!!'
+    ];
+
+    if (! array_key_exists($post, $posts)) {
+        abort(404, 'Sorry that post was not found.');
+    }
+
+    return view('post', [
+        'post' => $posts[$post]
+    ]);
+});
